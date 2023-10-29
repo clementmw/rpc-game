442aa6b40701cfdffb2df396391fcb745067b98d
clementmwamba <clementmwamba23@gmail.com>
2023-10-29 18:08:18 +0300

    4

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
+
+const values = ['rock','paper','scissors'];
+const buttons = document.querySelectorAll('.buttons')
+
 function getComputerChoice (){
-    let values = ['rock','paper','scissors'];
     return values;
 }
-//console.log(getComputerChoice())
 
-const buttons = document.querySelector('.container')
+let playerScore = 0;
+let computerScore = 0;
 
-buttons.addEventListener('click',
+
+
 function playRound(playerSelection, computerSelection){
-    alert ('clicked')
+   computerSelection === getComputerChoice()
 
-//   playerSelection = playerSelection. toLowerCase();
+
+        if(playerSelection === computerSelection){
+            console.log( 'Its a TIE! play again')
+        }
+        else if( (playerSelection === 'paper' && computerSelection === 'rock')){
+           console.log(`you win ${playerSelection } beats ${computerSelection }`) 
+        }
+        else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+            console.log (`you win ${playerSelection } beats ${computerSelection}` )
+        }
+        else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+            console.log (`you win ${playerSelection } beats ${computerSelection }` )
+        }
+       else {
+        console.log ( `you loose ${computerSelection} beats ${playerSelection}! better luck next time.`)
+       }
+
+       getResults()
+    
+}
+
+function getResults (){
+  let results = document.querySelector('#displayResults') 
+    results.innerHTML = ''
+    let resultdiv = document.createElement('div')
+    resultdiv.classList.add('game')
+    results.appendChild(resultdiv); 
         
-    
-//         if(playerSelection === computerSelection){
-//             return  'Its a TIE! play again'
-//         }
-//         else if( (playerSelection === 'paper' && computerSelection === 'rock')){
-//             return `you win ${playerSelection .toUpperCase()} beats ${computerSelection .toUpperCase()}` 
-//         }
-//         else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-//             return `you win ${playerSelection .toUpperCase()} beats ${computerSelection .toUpperCase()}` 
-//         }
-//         else if (playerSelection === 'rock' && computerSelection === 'scissors'){
-//             return `you win ${playerSelection .toUpperCase()} beats ${computerSelection .toUpperCase()}` 
-//         }
-//        else {
-//         return  `you loose ${computerSelection} beats ${playerSelection .toUpperCase()}! better luck next time.`
-//        }
-    
-     
+}
 
-})
+// function updateScore() {
+//     const scoreDisplay = document.querySelector('#score');
+//     scoreDisplay.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+// }
+       
+buttons.forEach(button => {
+    button.addEventListener('click', function (event) {
+     event.preventDefault()
+        playRound(button.dataset.selection);
+    });
+});
+
+// Initialize the game
+getResults();
+
+
+
+// function updateScore() {
+//     const scoreDisplay = document.querySelector('#score');
+//     scoreDisplay.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+// }
+
+// function announceWinner() {
+//     if (playerScore === 5) {
+//         displayResult('Congratulations! You are the winner!');
+//     } else if (computerScore === 5) {
+//         displayResult('Computer wins. Better luck next time.');
+//     }
+// }
+
+
+
 
 
 
